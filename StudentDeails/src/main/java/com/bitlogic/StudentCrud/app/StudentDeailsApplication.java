aee433f7947a46ca94a541fcca112a6239189345
vikasJawale <vikasjawale10@gmail.com>
2023-07-10 16:15:27 +0530

    initial commit

---
+++ StudentDeails/src/main/java/com/bitlogic/StudentCrud/app/StudentDeailsApplication.java
@@ -0,0 +1,13 @@
+package com.bitlogic.StudentCrud.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDeailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentDeailsApplication.class, args);
+	}
+
+}
---
+++ StudentDeails/src/test/java/com/bitlogic/StudentCrud/app/StudentDeailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitlogic.StudentCrud.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDeailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
