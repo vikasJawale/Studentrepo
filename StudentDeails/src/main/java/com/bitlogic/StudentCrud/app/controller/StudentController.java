e79bdb64c891b862971ce457e4682f62167f7c9f
vikasJawale <vikasjawale10@gmail.com>
2023-07-11 09:57:05 +0530

    mapping added

--- StudentDeails/src/main/java/com/bitlogic/StudentCrud/app/controller/StudentController.java
+++ StudentDeails/src/main/java/com/bitlogic/StudentCrud/app/controller/StudentController.java
@@ -1,6 +1,7 @@
 package com.bitlogic.StudentCrud.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,7 @@ public class StudentController {
 	
 	@Autowired
 	StudentService ss;
-	
+	@PostMapping("/save")
 	public Student savestudentDetails(@RequestBody Student stu)
 	{
 		return ss.savestudentDetails(stu);
